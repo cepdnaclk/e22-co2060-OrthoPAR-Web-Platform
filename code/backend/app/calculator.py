f35107ee65c0805953174dec914f7495eaea4481
daniel <daniel.adrian.victor@gmail.com>
2026-03-16 10:24:35 +0530

    added topbar

--- code/backend/app/calculator.py
+++
@@ -1,5 +0,0 @@
-#calculator for manual override of PAR score
-def calculate_par_score(landmarks):
-    # Placeholder for the actual PAR score calculation logic
-
-    return par_score
--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
 import SideBar from './components/SideBar'
 import TopBar from './components/TopBar' 
 import AuthPage from './pages/AuthPage'
@@ -9,13 +10,25 @@ import SettingsPage from './pages/SettingsPage'
 import AnalysisPage from './pages/AnalysisPage'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    console.log('Searching for:', query);
+    // You can add your search logic here later
+  };
   return (
+    
     <BrowserRouter>
       <div className="app-container">
       <SideBar />
 
       <div className="not-sidebar">
-      <TopBar /> 
+      <TopBar 
+        title="Dashboard" 
+        subtitle="Welcome back, Dr. Chen · Feb 17, 2025"
+        onSearch={handleSearch}
+      />
     
       <main className="main-content-skeleton">
         <Routes>
